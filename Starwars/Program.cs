e053b257b10d20e169465ad2b4790bed725ceda9
William <william.lignell@elev.ga.ntig.se>
2024-01-26 09:59:01 +0100

    StarwarsAPISearch

---
+++ Starwars/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using RestSharp;
+using System.IO;
+string yn = "y";
+while(yn == "yes" || yn == "y")
+{
+    Console.WriteLine("Write the number for the character you want to see");
+    string sWName = Console.ReadLine();
+    RestClient client = new("https://swapi.py4e.com/api/");
+
+
+    RestRequest request = new($"people/{sWName}/");
+
+    RestResponse response = client.GetAsync(request).Result;
+
+
+    if(response.StatusCode == System.Net.HttpStatusCode.OK)
+    {
+        SWCharacter s = JsonSerializer.Deserialize<SWCharacter>(response.Content);
+
+        Console.WriteLine(s.name);
+        Console.WriteLine(s.height);
+        Console.WriteLine(s.mass);
+        Console.WriteLine(s.hairColor);
+        Console.WriteLine(s.skinColor);
+    }
+    else
+    {
+        Console.WriteLine("What?");
+    }
+
+    // Console.WriteLine(response.Content);
+     File.WriteAllText("SWCharacter.json", response.Content);
+
+    Console.WriteLine();
+
+    Console.WriteLine("Do you want to see another character? yes/no");
+    yn = Console.ReadLine().ToLower();
+    Console.Clear();
+}
---
+++ Starwars/SWCharacter.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Text.Json.Serialization;
+
+public class SWCharacter
+{
+    [JsonPropertyName("name")]
+    public string name {get; set;}
+    [JsonPropertyName("height")]
+	public string height {get; set;}
+    [JsonPropertyName("mass")]
+	public string mass {get; set;}
+    [JsonPropertyName("hair_color")]
+    public string hairColor {get; set;}
+    [JsonPropertyName("skin_color")]
+    public string skinColor {get; set;}
+}
